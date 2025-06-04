c61f3fd5b5bf15271dcc1df49e49de24b60aabed
unknown <Administrador@lci-8-20.upf.br>
2025-06-04 20:42:37 -0300

    :sparkles: feat: 04/06/2025 night class

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -4,8 +4,14 @@ class CourseModel {
   String? description;
   String? startAt;
 
-  CourseModel({this.id, 
-              this.name, 
-              this.description, 
-              this.startAt});
+  CourseModel({this.id, this.name, this.description, this.startAt});
+
+  static CourseModel fromJson(Map<String, dynamic> json) {
+    return CourseModel(
+      id: json['id'] ?? '',
+      name: json['name'],
+      description: json['description'],
+      startAt: json['startAt'],
+    );
+  }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,1 @@
+const URL_MOCKAPI = "https://6840cac8d48516d1d3597297.mockapi.io";
